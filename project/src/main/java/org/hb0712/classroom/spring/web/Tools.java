54321d552513628726023d3199a3cdee8fb23d5f
sya <workspace>
2020-03-27 14:15:28 +0800

    删除了原来的格式，换成了maven格式

---
+++ project/src/main/java/org/hb0712/classroom/spring/web/Tools.java
@@ -0,0 +1,26 @@
+package org.hb0712.classroom.spring.web;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Tools {
+
+	
+	public void hashMap() {
+		Map map = new HashMap();
+	}
+	
+	public void linkedhashMap() {
+		Map map = new LinkedHashMap();
+	}
+	
+	public void treeMap() {
+		Map map = new TreeMap();
+	}
+
+	public static void main(String[] args) {
+		System.out.println(123);
+	}
+}
